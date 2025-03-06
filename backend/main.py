6602dc84127996d82f70da3ba4fc47e7ff832b93
Anton <137686641+chikibambony137@users.noreply.github.com>
2025-03-07 00:08:02 +0300

    fix cors bug

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes import router
-from config import FRONTEND_HOST, FRONTEND_PORT
+from config import FRONTEND_HOST, FRONTEND_PORT, TEST_IP
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[f"http://{FRONTEND_HOST}:{FRONTEND_PORT}"],
+    allow_origins=[
+        f"http://{FRONTEND_HOST}:{FRONTEND_PORT}",
+        f"http://{TEST_IP}"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
