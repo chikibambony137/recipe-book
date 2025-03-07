a1d9f71e8c9533949eb8365c9a4ed81a81bc6b05
Anton <137686641+chikibambony137@users.noreply.github.com>
2025-03-07 21:12:28 +0300

    test2

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
-DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
+DATABASE_URL = f'postgresql://postgres:0053@db/recipe-list'
 engine = create_engine(DATABASE_URL)
 
 def get_db():
